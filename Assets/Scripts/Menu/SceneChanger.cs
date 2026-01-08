938764c38027ca2fc2f39fcb8ada8976d60c7608
mystiified <152400721+mystiified@users.noreply.github.com>
2026-01-08 15:55:48 +0100

    Options button leads to separate Canvas.
    - Options button leads to separate canvas with no content.
    - Quit doesn't work yet.

--- Assets/Scripts/Menu/SceneChanger.cs
+++ Assets/Scripts/Menu/SceneChanger.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    [SerializeField] GameObject settingsCanvas;
+    [SerializeField] GameObject menuCanvas;
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void SwapToSettings()
+    {
+        menuCanvas.SetActive(false);
+        settingsCanvas.SetActive(true);
+    }
+
+    public void SwapToMainMenu()
+    {
+
     }
 }

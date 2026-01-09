d5e79edef2737209758866f836eaad6be99dd996
Pleblel <pelleaugust07@outlook.com>
2026-01-09 14:15:05 +0100

    Shoot

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum ShootPattern { Single, Double, Tracking}
+    ShootPattern pattern;
+    Transform firePoint;
+    float cooldown;
+    float fireRate;
+    BulletTypes[] bullets;
+    private void Start()
     {
-        
+        firePoint = GetComponentInChildren<Transform>().Find("Fire Point");
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.J) && Time.time >= cooldown)
+        {
+            cooldown = Time.time + 1f / fireRate;
+            Shoot();
+        }
     }
+
+    public void SetPattern(ShootPattern newPattern) => pattern = newPattern;
+
+    void Shoot()
+    {
+        switch (pattern)
+        {
+            case ShootPattern.Single:
+                break;
+            case ShootPattern.Tracking:
+                break;
+            case ShootPattern.Double:
+                break;
+        }
+    }
+}
+
+[System.Serializable]
+public struct BulletTypes
+{
+    public string name;
+    public float scale;
+    public GameObject bullet;
 }

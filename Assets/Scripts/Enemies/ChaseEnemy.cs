9fb514393ba70378c397f9185667a0588386cd29
Darren Scott <darrenpipi8@gmail.com>
2026-01-16 01:41:13 +0100

    GameScene

--- Assets/Scripts/Enemies/ChaseEnemy.cs
+++ Assets/Scripts/Enemies/ChaseEnemy.cs
@@ -24,7 +24,7 @@ public class ChaseMovement : EnemyEntity
     {
         FindPlayer();
         distanceStop = Random.Range(2, 6);
-        enemyHP = 40f;
+        enemyHP = 30f;
         currentHP = enemyHP;
         movementSpeed = 5f;
         stopDistance = distanceStop;
